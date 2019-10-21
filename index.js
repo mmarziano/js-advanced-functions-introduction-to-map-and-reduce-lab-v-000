70134419b24c4f8135785bdec00c62333ed2a903
Monica Marziano <mmarziano1@gmail.com>
2019-10-21 12:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  
+}
+
+function mapToNoChange(sourceArray) {
+  
+}
+
+function mapToDouble(sourceArray) {
+  
+}
+
+function mapToSquare(sourceArray) {
+  
+}
+
